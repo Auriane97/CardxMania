1ec4c34e75e42bff74254d334390dce8c8f6cf22
Auriane97 <auriane.francois@gmail.com>
2022-09-14 08:51:16 +0200

    exemplaireController get all + getCArtewithserie

--- CardXManiaBackEnd/src/main/java/cardxMania/service/AchatService.java
+++ CardXManiaBackEnd/src/main/java/cardxMania/service/AchatService.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import cardxMania.dao.IDAOAchat;
 import cardxMania.exception.AchatException;
--- CardXManiaBackEnd/src/main/java/cardxMania/service/CarteService.java
+++ CardXManiaBackEnd/src/main/java/cardxMania/service/CarteService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import cardxMania.dao.IDAOCarte;
 import cardxMania.exception.CarteException;
